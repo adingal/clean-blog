14cdf0ffbed6b320d940666234852b10ede981f1
Alex Dingal <lhexie2014@gmail.com>
2023-02-19 02:23:51 +0800

    Remove dependency and update imports

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const UserSchema = new Schema({
